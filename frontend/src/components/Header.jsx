d9881c5db1ac09c1489e6b6dc08655fe7d64ef0a
ikjasrasool <ikjasrasool2022@gmail.com>
2025-05-12 20:03:54 +0530

    add some

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -22,19 +22,20 @@ const Header = () => {
             <ul className="flex space-x-6 items-center">
               <li><Link to="/" className={activeTab === '/' ? 'text-red-600 font-bold' : 'text-gray-600'}>Home</Link></li>
               <li><Link to="/menu" className={activeTab === '/menu' ? 'text-red-600 font-bold' : 'text-gray-600'}>Menu</Link></li>
-              <li><Link to="/about" className={activeTab === '/about' ? 'text-red-600 font-bold' : 'text-gray-600'}>About</Link></li>
-              <li><Link to="/contact" className={activeTab === '/contact' ? 'text-red-600 font-bold' : 'text-gray-600'}>Contact</Link></li>
-              <li><Link to="/OpenMapsToHotel" className={activeTab === '/OpenMapsToHotel' ? 'text-red-600 font-bold' : 'text-gray-600'}>Track Location</Link></li>
               <li>
                 <Link to="/cart" className="relative text-gray-700 hover:text-red-600">
                   🛒
                   {totalItems > 0 && (
-                    <span className="absolute -top-2 -right-3 bg-red-600 text-white text-xs rounded-full px-1">
+                      <span className="absolute -top-2 -right-3 bg-red-600 text-white text-xs rounded-full px-1">
                       {totalItems}
                     </span>
                   )}
                 </Link>
               </li>
+              <li><Link to="/about" className={activeTab === '/about' ? 'text-red-600 font-bold' : 'text-gray-600'}>About</Link></li>
+              <li><Link to="/contact" className={activeTab === '/contact' ? 'text-red-600 font-bold' : 'text-gray-600'}>Contact</Link></li>
+              <li><Link to="/OpenMapsToHotel" className={activeTab === '/OpenMapsToHotel' ? 'text-red-600 font-bold' : 'text-gray-600'}>Track Location</Link></li>
+
             </ul>
           </nav>
         </div>
